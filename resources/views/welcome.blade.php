806210be968c25261467219924ddaf900ac09a18
Angelina0310 <48234899+Angelina0310@users.noreply.github.com>
2019-12-26 18:08:40 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel-test
+                    Love-tesy^_^)
                 </div>
 
                 <div class="links">
